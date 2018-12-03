8c25ed2daa3c65c87e978b220b64fa906b6fd272
Mchoffn <mchoffn@hotmail.com>
2018-12-03 12:58:38 +0100

    update efter delete
    update efter delete

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Projekt</title>
+
+	<!--Responsive tag-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--Favicon-->
+	<link rel="shortcut icon" href="images/fa-image.png">
+
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<!--css-->
+	<link rel="stylesheet" href="stylesheet/style.css">
+</head>
+
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">(logo/navn)</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+		
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<a class="nav-link">Logget ind som: (navn 'rolle')<span class="sr-only"></span></a>
+					<button class="btn btn-outline-danger">Log ud</button>
+			</div>
+		</nav>
+		<!--Navbar end-->
+		
+		<!--Add new catagory-->
+		<div class="text-center my-auto">
+			<div class="font-weight-bold">Tilføj ny katogori</div>
+			<a href="new-catagory/index.php"><button class="btn btn-primary btn-lg mx-auto">+</button</a>
+			</div>
+	</div>
+</body>
+</html>
---
+++ view-videos/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Projekt</title>
+
+	<!--Responsive tag-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--Favicon-->
+	<link rel="shortcut icon" href="../images/fa-image.png">
+
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<!--css-->
+	<link rel="stylesheet" href="../stylesheet/style.css">
+</head>
+
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="../index.php">(logo/navn)</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+		
+
+
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<a class="nav-link">Logget ind som: (navn 'rolle')<span class="sr-only"></span></a>
+					<button class="btn btn-outline-danger">Log ud</button>
+			</div>
+		</nav>
+		<!--Navbar end-->
+		<div class="container mt-3 mb-3">
+			<div class="row">
+
+				<div class="media col-xl-6 pt-3 border video-list">
+					<img class="align-self-start mr-3" src="../images/fa-image.png" width="64" alt="Generic placeholder image">
+					<div class="media-body">
+						<h5 class="mt-0">Top-aligned media</h5>
+						<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						<hr>
+						<!--New video-->
+						<img class="align-self-start mr-3" src="../images/fa-image.png" width="64" alt="Generic placeholder image">
+						<h5 class="mt-0">Top-aligned media</h5>
+						<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						<hr>
+				</div>
+			</div>
+				
+				<div class="col-xl-4 col-sm-12 mb-3">
+					<div class="font-weight-bold">vid-title</div>
+					<img src="https://via.placeholder.com/300x300">
+				</div>
+		</div>
+</body>
+</html>
