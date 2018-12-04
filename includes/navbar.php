af8ff3057b81aba96ed6988f81b7b72fca27f0c4
Adnan-Bacic <adnan-bacic@hotmail.com>
2018-12-04 13:19:57 +0100

    include folder, changed file locations

---
+++ includes/navbar.php
@@ -0,0 +1,16 @@
+<!--Navbar start-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="../index.php"><img src="../images/sunset-favicon.png" width="75"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav ml-auto">
+			<a class="nav-link text-dark">Logget ind som: *username* (*role*)<span class="sr-only"></span></a>
+			<button class="btn btn-outline-danger">Log ud</button>
+	</div>
+</nav>
+<!--Navbar end-->
--- view-videos/index.php
+++ view-videos/index.php
@@ -1,43 +1,14 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Projekt</title>
-
-	<!--Responsive tag-->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!--Favicon-->
-	<link rel="shortcut icon" href="../images/fa-image.png">
-
-	<!--Bootstrap-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-	<!--css-->
-	<link rel="stylesheet" href="../stylesheet/style.css">
-</head>
-
+	<?php
+		include '../includes/head.php';
+	?>
 <body>
 	<div class="container-fluid">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="../index.php">(logo/navn)</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-		
-
-
-
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav ml-auto">
-					<a class="nav-link">Logget ind som: (navn 'rolle')<span class="sr-only"></span></a>
-					<button class="btn btn-outline-danger">Log ud</button>
-			</div>
-		</nav>
-		<!--Navbar end-->
+		<?php
+			include '../includes/navbar.php';
+		?>
 		<div class="container mt-3 mb-3">
 			<div class="row">
 
@@ -52,13 +23,47 @@
 						<h5 class="mt-0">Top-aligned media</h5>
 						<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
 						<hr>
+						
+					</div>
 				</div>
-			</div>
-				
+
 				<div class="col-xl-4 col-sm-12 mb-3">
 					<div class="font-weight-bold">vid-title</div>
 					<img src="https://via.placeholder.com/300x300">
 				</div>
+			</div>
+			
+			<!--add new video-->
+			<div class="row">
+			<h2 class="mx-auto mt-3">Tilføj ny video</h2>
 		</div>
+		<div class="row">
+			<div class="mx-auto">
+				<form class="mt-3" action="" autocomplete="off">
+					<div class="form-input">
+						<div class="font-weight-bold input-txt">Titel: </div>
+						<input class="input-field" type="text" name="video_title" required>
+					</div>
+					<br>
+					<div class="form-input">
+						<div class="font-weight-bold input-txt">Beskrivelse: </div>
+						<input class="input-field" type="text" name="video_description" required>
+					</div>
+					<br>
+					<div class="custom-file mb-3">
+  <input type="file" accept="video/mp4" class="custom-file-input" id="customFile" required>
+  <label class="custom-file-label" for="customFile">Vælg video (.mp4)</label>
+</div>
+					<br>
+					<button class="btn btn-primary btn-block" value="new_video">Tilføj</button>
+				</form>
+			</div>
+		</div>
+			</div>
+			<?php
+			include '../includes/footer.php';
+		?>
+		
+	</div>
 </body>
 </html>
