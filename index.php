7fe5cd3046d9df21255039aecc450e81eaf12a6d
Adnan-Bacic <you@example.com>
2018-12-14 22:34:15 +0100

    commit

--- index.php
+++ index.php
@@ -6,30 +6,27 @@
 	?>
 
 </head>
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
 		<div class="mx-auto">
 			<h1 class="form-signin-heading">Lean Up</h1>
 			<img class="pic" class="center" src="images/book.png">
 		</div>
 	</div>
-</div>
-	
-	
+
 
   <div class="wrapper">
-    <form class="form-signin" action="login.php" method="post">       
+    <form class="form-signin" action="login.php" method="post" autocomplete="off">       
       <h2 class="form-signin-heading">Medlem</h2>
       <input type="text" class="form-control" name="un" placeholder="Brugernavn" required autofocus>
       <input type="password" class="form-control" name="pw" placeholder="Password" required>      
-
       <button class="btn btn-lg btn-primary btn-block" type="submit" value="Log ind">Login</button>   
     </form>
   </div>
 
-	
 	<?php
 	include 'includes/footer.php';
 	?>
+	</div>
 </body>
 </html>
--- view-videos.php
+++ view-videos.php
@@ -2,141 +2,132 @@
 <html>
 <head>
 	<?php
-		include 'includes/head.php';
+	include 'includes/head.php';
 	?>
-<body>
-	<div class="container-fluid">
-		<?php
+
+	<body>
+		<div class="container-fluid">
+			<?php
 			include 'includes/navbar.php';
-		?>
-		
+			?>
+
 			<h2 class="text-center mt-3">Videoer:</h2>
-				<!-- Start video container -->
-		<div class="row d-flex justify-content-around mt-2">
-				
-							<!--Showing all videos -->
-			<div class="col-xl-4 col-sm-12 mb-3">
-									<?php
-									require_once( 'database-connect/dbcon.php' );
+			<!-- Start video container -->
+			<div class="row d-flex justify-content-around mt-2">
 
-									$sql = 'SELECT id as vid, title, description, location FROM ss_videos ORDER BY id DESC';
-									$stmt = $link->prepare($sql);
-									$stmt->bind_result($vid, $title, $description, $location);
-									$stmt->execute();
-									while ($stmt->fetch()) {
-										
-									
-										?>
-					
-		<div class="col">
-				
-			
-					<div class="category-item col-xl-3 col-sm-12 border border-primary">
+				<!--Showing all videos -->
+				<div class="col-xl-4 col-sm-12 mb-3">
+					<?php
+					require_once( 'database-connect/dbcon.php' );
 
-				<h2 class="d-inline">
-					<?= $title ?>
-						<?php
-					if(isset($_SESSION['role'])){
-								?>
-				
-						
-					<div class="delete d-inline float-right">
-							<form action="delete-video.php" method="post">
-									<input type="hidden" name="vid" value="<?=$vid?>">
-							<div class="delete-img">
-									<input type="image" src="images/delete-img.png" title="Delete" width="20" height="20" alt="Delete">
-							</div>
-							</form>
-					
-				
-				<?php } ?>
-			</h2>
-				<p>
-				<?= $description ?>
-				</p>
-				<?php echo "<div >";
+					$sql = 'SELECT id as vid, title, description, location FROM ss_videos ORDER BY id DESC';
+					$stmt = $link->prepare( $sql );
+					$stmt->bind_result( $vid, $title, $description, $location );
+					$stmt->execute();
+					while ( $stmt->fetch() ) {
+
+
+						?>
+
+					<div class="col border border-primary">
+
+
+						<div class="category-item col-xl-3 col-sm-12">
+
+							<h2 class="d-inline">
+								<?= $title ?>
+								<?php
+								if ( isset( $_SESSION[ 'role' ] ) ) {
+									?>
+								<div class="delete d-inline float-right">
+									<form action="delete-video.php" method="post">
+										<input type="hidden" name="vid" value="<?=$vid?>">
+										<div class="delete-img">
+											<input type="image" src="images/delete-img.png" title="Delete" width="20" height="20" alt="Delete">
+										</div>
+									</form>
+									<?php } ?>
+							</h2>
+							<p>
+								<?= $description ?>
+							</p>
+							<?php echo "<div >";
 										echo "<video src='".$location."' controls width='320px' height='200px' >";
 										echo "</div>"; 
 										?>
-										</h2>
-							
-		<?php
-				if(isset($_SESSION['role'])){
+							</h2>
+
+							<?php
+							if ( isset( $_SESSION[ 'role' ] ) ) {
+								?>
+
+							<?php
+							} else {
+
+							}
 							?>
-			
-					<?php
-					} else {
-																							
-				}
-					?>
-							
-							
-		</div>
-			
-
-			<?php
-
-			//echo "Titel: " . $row[ "title" ] . "<br>" . "Beskrivelse: " . $row[ "description" ]. "<br>" . " Forhåndsvisning: " . $row[ "thumbnail" ] . "<br>".PHP_EOL;
-			
-
-			}
-			$link->close();
-			?>
 
 
+							</div>
 
 
-</div>
+							<?php
+
+							//echo "Titel: " . $row[ "title" ] . "<br>" . "Beskrivelse: " . $row[ "description" ]. "<br>" . " Forhåndsvisning: " . $row[ "thumbnail" ] . "<br>".PHP_EOL;
+
+
+							}
+							$link->close();
+							?>
+						</div>
 					</div>
+				</div>
 			</div>
-	</div>
 
-			
-			
+
+
 			<?php
-		if(isset($_SESSION['role'])){
+			if ( isset( $_SESSION[ 'role' ] ) ) {
 				?>
 			<!--add new video-->
 			<div class="row justify-content-center">
-			<h2 class="">Tilføj ny video</h2>
+				<h2 class="">Tilføj ny video</h2>
 			</div>
 			<br>
-		
-							<!-- dette er kassen med oprettelse af nye vidoer-->
-						<div class="row border" id="tilfojvideo">
-							<div class="mx-auto">
-								<form class="" method="post" action="create-video.php" autocomplete="off" enctype="multipart/form-data">
-								<div class="form-input">
-									<div class="font-weight-bold input-txt text-warning">Titel: </div>
-								<input class="input-field" type="text" name="video_title" required>
-							</div>
-								<br>
+
+			<!-- dette er kassen med oprettelse af nye vidoer-->
+			<div class="row border" id="tilfojvideo">
+				<div class="mx-auto">
+					<form class="" method="post" action="create-video.php" autocomplete="off" enctype="multipart/form-data">
+						<div class="form-input">
+							<div class="font-weight-bold input-txt text-warning mt-3">Titel: </div>
+							<input class="input-field" type="text" name="video_title" required>
+						</div>
+						<br>
 						<div class="form-input">
 							<div class="font-weight-bold input-txt text-warning">Beskrivelse: </div>
 							<input class="input-field" type="text" name="video_description" required>
 						</div>
-					<br>
-					<div class="custom-file mb-3">
-  		<input type="file" name="video_title" accept="video/mp4" class="custom-file-input" id="customFile" required>
-  		<label class="custom-file-label" for="customFile">Vælg video (.mp4)</label>
-		</div>
-					<br>
-					<input class="font-weight-bold btn btn-warning btn-block mb-3 text-success" name="btnaddvideo" type="submit" value="Tilføj">
-				</form>
+						<br>
+						<div class="custom-file mb-3">
+							<input type="file" name="video_title" accept="video/mp4" class="custom-file-input" id="customFile" required>
+							<label class="custom-file-label" for="customFile">Vælg video (.mp4)</label>
+						</div>
+						<br>
+						<input class="font-weight-bold btn btn-warning btn-block mb-3 text-success" name="btnaddvideo" type="submit" value="Tilføj">
+					</form>
+				</div>
 			</div>
-		</div>
-	</div>
-		
-			
-		<?php
+
+
+			<?php
+			} else {
+
 			}
-		else {
-			
-		}
-		?>
+			?>
 			<?php
 			include 'includes/footer.php';
-		?>
+			?>
 		</div>
-</body>
+	</body>
 </html>
